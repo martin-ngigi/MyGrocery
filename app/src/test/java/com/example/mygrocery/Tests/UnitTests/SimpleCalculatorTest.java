fb3808097b6b037afd32ee71f20b8c9a597c0117
martin-ngigi <martinwainaina001@gmail.com>
2022-10-24 10:46:32 +0300

    This test should pass since 10==10

--- app/src/test/java/com/example/mygrocery/Tests/UnitTests/SimpleCalculatorTest.java
+++ app/src/test/java/com/example/mygrocery/Tests/UnitTests/SimpleCalculatorTest.java
@@ -30,7 +30,7 @@ public class SimpleCalculatorTest {
     public void threePlusSevenEqualsTen(){
         SimpleCalculator calculator = new SimpleCalculator();
         int sum = calculator.add(3,7);
-        assertEquals(11, sum); // 10==10 .... This test should fail since 11 != 10
+        assertEquals(10, sum); // 10==10 .... This test should pass since 10 == 10
 
     }
 }
